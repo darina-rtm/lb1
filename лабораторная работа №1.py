528175785d088b0ede157c10eeb7f6e33dfc935c
darina-rtm <darina-rtm@yandex.ru>
2024-10-08 11:30:06 +0300

    Delete лабораторная работа №1.py

--- лабораторная работа №1.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(text):
-  text = text.lower().replace(" ", "")
-  return text == text[::-1]
-
-text = input("Введите текст: ")
-if is_palindrome(text):
-  print("Это палиндром!")
-else:
-  print("Это не палиндром.")
